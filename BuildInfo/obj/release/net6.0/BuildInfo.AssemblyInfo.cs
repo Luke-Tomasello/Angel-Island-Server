44fb774a5be8ed0c78a631d89f0598a718af36c1
Luke Tomasello <53952801+Luke-Tomasello@users.noreply.github.com>
2024-07-30 12:00:18 -0700

    completing first check in

--- BuildInfo/obj/release/net6.0/BuildInfo.AssemblyInfo.cs
+++ BuildInfo/obj/release/net6.0/BuildInfo.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuildInfo")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d91c7264e386c99187853c2615b76a36d655345f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuildInfo")]
